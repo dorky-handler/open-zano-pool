9a4d372d3f93392af902ad7f9764b7a93dc4264a
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2021-07-05 04:23:42 +0530

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -11,9 +11,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/zano-mining/open-zano-pool/rpc"
-	"github.com/zano-mining/open-zano-pool/storage"
-	"github.com/zano-mining/open-zano-pool/util"
+	"github.com/dorky-handler/open-zano-pool/rpc"
+	"github.com/dorky-handler/open-zano-pool/storage"
+	"github.com/dorky-handler/open-zano-pool/util"
 )
 
 const txCheckInterval = 5 * time.Second
