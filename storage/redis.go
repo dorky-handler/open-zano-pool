79e9103a916457b49e36adef8f3598ca3160bd76
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2021-09-21 16:13:44 +0530

    changed zslice to int

--- storage/redis.go
+++ storage/redis.go
@@ -755,7 +755,7 @@ func (r *RedisClient) CollectWorkersStats(nWindow,sWindow, lWindow time.Duration
 	if err != nil {
 		return nil, err
 	}
-	totalshares :=  cmds[2].(*redis.ZSliceCmd)
+	totalshares :=  cmds[2].(*redis.IntCmd)
 	totalHashrate := int64(0)
 	currentHashrate := int64(0)
 	reportedHashrate := int64(0)
