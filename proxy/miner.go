49319cd3459e84b403f6df99b91d5a9cc9cf06f9
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2021-09-30 23:02:27 +0530

    rolled back test changes

--- proxy/miner.go
+++ proxy/miner.go
@@ -104,25 +104,14 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 			log.Printf("Block found by miner %v@%v at height %d", login, ip, h.height)
 		}
 	} else {
-		/*exist, err := s.backend.WriteShare(login, id, share_params[:3], shareDiff, h.height, s.hashrateExpiration , ip)
+		exist, err := s.backend.WriteShare(login, id, share_params[:3], shareDiff, h.height, s.hashrateExpiration , ip)
 		if exist {
 			return true, false
 		}
 		if err != nil {
 			log.Println("Failed to insert share data into backend:", err)
 		}
-		*/
-		exist, err := s.backend.WriteBlock(login, id, block_params[:3], shareDiff, h.diff.Int64(), h.height, s.hashrateExpiration , ip)
-			if exist {
-				return true, false
-			}
-			if err != nil {
-				log.Println("Failed to insert block candidate into backend:", err)
-			} else {
-				log.Printf("Inserted block %v to backend", h.height)
-			}
-			log.Printf("Block found by miner %v@%v at height %d", login, ip, h.height)
-		
+				
 	}
 	return false, true
 }
