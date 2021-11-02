c7aa48d46eeebf85c63b37dfbaacd25a2813ecfb
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2021-11-02 15:36:34 +0530

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -26,7 +26,7 @@ func (s *ProxyServer) handleLoginRPC(cs *Session, params []string, id string) (b
 	}
 	loginarray := []byte(login)
 	encoding := base58.BitcoinEncoding
-	tempstr, err := encoding.Decode(loginarray)
+	_, err := encoding.Decode(loginarray)
 	log.Printf("Malformed wallet string error is %v",err)
 	if err != nil {
 		return false, &ErrorReply{Code: -1, Message: "Invalid Wallet address"}
