9170c90d29f1e66b0d98f3e85db99ead6292976d
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2021-11-03 23:27:38 +0530

    Update rpc.go

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -354,7 +354,7 @@ func (r *RPCClient) GetBalance() (*big.Int, error) {
 }
 
 
-func (r *RPCClient) WalletCheck(params []string) (bool) {
+func (r *RPCClient) WalletCheck(params string) (bool) {
 	tempstrr := map[string]string{"integrated_address": params}
 	rpcResp, err := r.doPost(r.Wurl, "split_integrated_address", tempstrr)
 	if err != nil {
